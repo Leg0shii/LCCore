43d941780468cae7fc524bb75516f42ea5e8e82a
Dkay <dklink750@gmail.com>
2024-03-11 14:20:32 -0400

    fix self message command

---
+++ src/main/java/de/legoshi/lccore/command/chat/SelfMessageCommand.java
@@ -0,0 +1,33 @@
+package de.legoshi.lccore.command.chat;
+
+import de.legoshi.lccore.manager.ChatManager;
+import de.legoshi.lccore.util.Register;
+import de.legoshi.lccore.util.Utils;
+import de.legoshi.lccore.util.message.Message;
+import de.legoshi.lccore.util.message.MessageUtil;
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
+import me.fixeddev.commandflow.annotated.annotation.OptArg;
+import me.fixeddev.commandflow.stack.ArgumentStack;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import team.unnamed.inject.Inject;
+
+@Register
+@Command(names = {"sm", "selfmsg"}, permission = "selfmsg", desc = "[message]")
+public class SelfMessageCommand implements CommandClass {
+
+    @Inject private ChatManager chatManager;
+
+    @Command(names = "")
+    public void selfMessage(CommandSender sender, @OptArg String first, @OptArg ArgumentStack as) {
+        if (!(sender instanceof Player)) {
+            MessageUtil.send(Message.NOT_A_PLAYER, sender);
+            return;
+        }
+
+        Player p = (Player) sender;
+        p.sendMessage("* " + chatManager.globalChat(p, Utils.joinArguments(first, as)));
+    }
+}
