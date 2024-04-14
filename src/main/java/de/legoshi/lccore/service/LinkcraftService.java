8e2524fb549844b40a1463be1abf67927ab470b7
Dkay <dklink750@gmail.com>
2024-04-13 22:56:19 -0400

    auto register listeners

--- src/main/java/de/legoshi/lccore/service/LinkcraftService.java
+++ src/main/java/de/legoshi/lccore/service/LinkcraftService.java
@@ -8,11 +8,25 @@ import team.unnamed.inject.Named;
 public class LinkcraftService implements Service {
     @Inject @Named("command") private Service commandService;
     @Inject @Named("papi") private Service papiService;
+    @Inject @Named("config") private Service configService;
+    @Inject @Named("database") private Service dbService;
+    @Inject @Named("listener") private Service listenerService;
 
     @Override
     public void start() {
-        MessageUtil.log(Message.SERVICE_STARTED, true, "plugin");
-        commandService.start();
-        papiService.start();
+        startService(configService, "Configuration");
+        startService(dbService, "Database");
+        startService(commandService, "Command");
+        startService(listenerService, "Listener");
+        startService(papiService, "PAPI");
+    }
+
+    private void startService(Service service, String display) {
+        long startTime = System.nanoTime();
+        MessageUtil.log(Message.SERVICE_STARTING, true, display);
+        service.start();
+        long endTime = System.nanoTime();
+        double elapsedTimeInSeconds = (endTime - startTime) / 1_000_000_000.0;
+        MessageUtil.log(Message.SERVICE_STARTED, true, display, String.format("%.1f", elapsedTimeInSeconds));
     }
 }
