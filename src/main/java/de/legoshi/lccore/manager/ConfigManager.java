b1e81972b40ad30e0c84214746f647f49d2812d7
Dkay <dklink750@gmail.com>
2024-11-08 13:33:24 -0500

    disable achievements for prod

--- src/main/java/de/legoshi/lccore/manager/ConfigManager.java
+++ src/main/java/de/legoshi/lccore/manager/ConfigManager.java
@@ -77,8 +77,7 @@ public class ConfigManager {
             e.printStackTrace();
         }
 
-        // TODO: comment this for deployment
-        achievementManager.loadAchievements();
+        //achievementManager.loadAchievements();
 
         loadMessages();
         if(!first) {
